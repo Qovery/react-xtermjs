b605b11d38c30615ebc315d5b49a88de40c23e9d
Remi Bonnet <bonnet.rem@gmail.com>
2024-05-02 16:46:21 +0200

    chore: init library

---
+++ README.md
@@ -0,0 +1,26 @@
+<br  />
+
+<h1>XTerm.js for React</h1>
+
+<p>
+    A React library to use the powerful of <a href="https://github.com/xtermjs/xterm.js" target="_blank">Xterm.js</a>
+</p>
+<br />
+
+## Getting Started
+
+### Installation
+
+NPM:
+
+    npm install react-xtermjs
+
+Yarn:
+
+    yarn add react-xtermjs
+
+
+<br />
+<hr />
+
+<p>react-xtermjs is initiated and maintained by <a href="https://qovery.com" target="blank">Qovery</a>.</p>
---
+++ rollup.config.mjs
@@ -0,0 +1,36 @@
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import commonjs from '@rollup/plugin-commonjs'
+import terser from '@rollup/plugin-terser'
+import postcss from 'rollup-plugin-import-css'
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/bundle.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/bundle.min.js',
+        format: 'iife',
+        name: 'version',
+        plugins: [terser()],
+      },
+    ],
+    plugins: [
+      resolve({
+        extensions: ['.ts', '.tsx'],
+        skip: ['react', 'react-dom'],
+      }),
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.json',
+      }),
+      postcss({ extensions: ['.css'], inject: true, extract: false }),
+    ],
+    external: ['react', 'react-dom', 'react/jsx-runtime'],
+  },
+]
